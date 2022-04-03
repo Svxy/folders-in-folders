ff7ee7dc5945e92fb0fc06724d7d9d1e5b2a80cf
TnyavnToツ゚ <93295911+Svxy@users.noreply.github.com>
2022-04-03 09:25:09 -0400

    More Advanced Now

--- @echo off.bat
+++ @echo off.bat
@@ -1,7 +1,23 @@
 @echo off
 
-:sexyman
-md NigNogPollyWog
-echo NigNogPollyWog made
-cd NigNogPollyWog
-goto sexyman
+color 0C
+echo:
+echo:
+echo                                          WARNING;
+echo:
+echo:
+echo        THIS SCRIPT WILL MAKE OVER 5000 FOLDERS IF LEFT RUNNING FOR ABOVE 15 SECONDS
+echo:
+echo:
+echo:
+ping localhost -n 6 >nul
+cls
+
+color 0A
+:t1
+md TnyavnTo
+echo:
+echo                           Folder made
+echo:
+cd TnyavnTo
+goto t1
